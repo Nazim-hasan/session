ee80e6b67110ca283c95cc805627cee86bf2b876
Nazim Hasan <nazimhasan124073@gmail.com>
2021-09-07 22:09:41 +0600

    update

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const additem = ()=>{
-    const productValue=document.getElementById('product-name');
+    let productValue=document.getElementById('product-name');
     const productName=productValue.value;
+    if(!productName){
+        return;
+    }
     //display product 
     displayProduct(productName);
-
-    productValues.value='';
+    addProductToCart(productName);
+    productValue.value='';
 }
 const displayProduct=name=>{
     const ul=document.getElementById('items');
     const li=document.createElement('li');
     li.innerText=name;
     ul.appendChild(li);
+}
+
+
+const getCart = () =>{
+    const cart=localStorage.getItem('cart');
+    let cartObject;
+    if(cart){
+        cartObject=JSON.parse(cart);
+    }
+    else{
+        cartObject={};
+    }
+    return cartObject;
+}
+
+const addProductToCart=name=>{
+    const cart=getCart();
+    cart[name]=1;
+    const cartStringified=JSON.stringify(cart);
+    localStorage.setItem('cart',cartStringified);
 }
